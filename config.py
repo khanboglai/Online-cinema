2e752b97bca577103b316bcb9156a3a2cbe29302
lexasy <lexav2016@mail.ru>
2024-11-13 23:20:23 +0300

    Авторизация версия 1.0

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+"""Config"""
 import os
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    """Settings"""
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
@@ -12,11 +14,16 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
-    )
+        env_file=os.path.join(
+            os.path.dirname(
+                os.path.abspath(__file__)),
+            ".env"))
+
 
 settings = Settings()
 
+
 def get_db_url():
+    """Getter of db URL"""
     return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
             f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
--- main.py
+++ main.py
@@ -1,36 +1,34 @@
-from fastapi import FastAPI, status, Depends, HTTPException
-from starlette.staticfiles import StaticFiles
-
-from models import models
-from repository.database import engine, SessionLocal
+"""App"""
 from typing import Annotated
+from fastapi import FastAPI, status, Depends
+from fastapi.responses import RedirectResponse
+from starlette.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
-from routers import auth
+from repository.database import SessionLocal
+from routers import auth, pages
 from routers.auth import get_current_user
-from fastapi.templating import Jinja2Templates
-from routers.pages import router as pages_router
 
 app = FastAPI()
+
 app.include_router(auth.router)
-app.include_router(pages_router)
-# Директория для шаблонов
+app.include_router(pages.router)
+
 app.mount('/static', StaticFiles(directory='static'), 'static')
 
-# models.Base.metadata.create_all(bind=engine)
+# def get_db():
+#     """Setting connection with database"""
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# DBDependency = Annotated[Session, Depends(get_db)]
+UserDependency = Annotated[dict, Depends(get_current_user)]
 
-db_dependency = Annotated[Session, Depends(get_db)]
-user_dependency = Annotated[dict, Depends(get_current_user)]
-
-# сюда шаблон без дизайна можно пока, просто чтобы там какой нибудь привет мир висел
 @app.get("/", status_code=status.HTTP_200_OK)
-async def user(user: user_dependency, db: db_dependency):
+async def user_auth(user: UserDependency):
+    """Redirecting by cookies"""
     if user is None:
-        raise HTTPException(status_code=401, detail='Authentification Failed')
-    return {"User": user}
+        return RedirectResponse(url="/login")
+    return RedirectResponse(url="/home", status_code=status.HTTP_302_FOUND)
