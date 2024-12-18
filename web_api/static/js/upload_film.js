dc827d1223cdd241641a70ceea8bf02e93643af1
lexasy <lexav2016@mail.ru>
2024-12-19 01:48:28 +0300

    Добавил прогрузочное колесо на странице загрузки фильма

--- web_api/static/js/upload_film.js
+++ web_api/static/js/upload_film.js
@@ -1,56 +1,59 @@
 async function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const movieName = document.getElementById('filmName').value;
-    // const director = document.getElementById('director').value;
-    const year = document.getElementById('year').value;
-    const country = document.getElementById('country').value;
-    const movieFile = document.getElementById('filmFile').files[0];
+  const movieName = document.getElementById('filmName').value;
+  const year = document.getElementById('year').value;
+  const country = document.getElementById('country').value;
+  const movieFile = document.getElementById('filmFile').files[0];
 
-    const selectedRating = document.querySelector('input[name="age_rating"]:checked');
-    const rating = selectedRating ? selectedRating.value : null;
+  const selectedRating = document.querySelector('input[name="age_rating"]:checked');
+  const rating = selectedRating ? selectedRating.value : null;
 
-    if (!rating) {
-        alert('Please select an age rating');
-        return;
-    }
+  if (!rating) {
+      alert('Please select an age rating');
+      return;
+  }
 
-    if (!movieName || !year || !country || !movieFile) {
-        alert('Please fill in all required fields');
-        return;
-    }
+  if (!movieName || !year || !country || !movieFile) {
+      alert('Please fill in all required fields');
+      return;
+  }
 
-    // Создание FormData для отправки данных
-    const formData = new FormData();
-    formData.append('film_name', movieName);
-    formData.append('age_rating', rating);
-    // formData.append('director', director);
-    formData.append('year', year);
-    formData.append('country', country);
-    formData.append('film_file', movieFile);
+  // Показать индикатор загрузки
+  const loadingIndicator = document.getElementById('loadingIndicator');
+  loadingIndicator.style.display = 'flex';
 
-    console.log(formData);
+  // Создание FormData для отправки данных
+  const formData = new FormData();
+  formData.append('film_name', movieName);
+  formData.append('age_rating', rating);
+  formData.append('year', year);
+  formData.append('country', country);
+  formData.append('film_file', movieFile);
 
-    try {
-        const response = await fetch('http://localhost:8000/upload', {
-            method: 'POST',
-            body: formData,
-        });
+  try {
+      const response = await fetch('http://localhost:8000/upload', {
+          method: 'POST',
+          body: formData,
+      });
 
-        if (!response.ok) {
-            const errorResponse = await response.json();
-            console.error('Server Error:', errorResponse);
-            alert('Failed to upload the film. Please try again.');
-            return;
-        }
+      if (!response.ok) {
+          const errorResponse = await response.json();
+          console.error('Server Error:', errorResponse);
+          alert('Failed to upload the film. Please try again.');
+          return;
+      }
 
-        const result = response.ok;
-        console.log('Upload successful:', result);
-        window.location.href = "http://localhost:8000/home";
-    } catch (error) {
-        console.error('Network Error:', error);
-        alert('An error occurred. Please check your internet connection and try again.');
-    }
+      const result = response.ok;
+      console.log('Upload successful:', result);
+      window.location.href = "http://localhost:8000/home";
+  } catch (error) {
+      console.error('Network Error:', error);
+      alert('An error occurred. Please check your internet connection and try again.');
+  } finally {
+      // Скрыть индикатор загрузки
+      loadingIndicator.style.display = 'none';
+  }
 }
 
 
